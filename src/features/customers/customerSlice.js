1ffe2b4043ec0ba59746c007b42948ab5faee186
Florent <florent.sylejmani@student.uni-pr.edu>
2025-11-28 14:12:33 +0100

    Add Redux Toolkit integration; refactor account and customer slices, and update store configuration

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,9 +1,42 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalId: "",
   createdAt: "",
 };
 
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalId) {
+        return {
+          payload: {
+            fullName,
+            nationalId,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalId = action.payload.nationalId;
+        state.createdAt = action.payload.createdAt;
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload.updateName;
+    },
+  },
+});
+
+export const {createCustomer,updateName} = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
 export default function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
     case "customer/createCustomer":
@@ -36,3 +69,4 @@ export function updateName(fullName) {
     payload: fullName,
   };
 }
+*/
